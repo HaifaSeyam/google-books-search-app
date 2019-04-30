f9326d784bae26a0b4b58c5e361c73f235fe285f
HaifaSeyam <h.abuamro@hotmail.com>
2019-04-30 09:11:59 -0500

    add the app

---
+++ client/build/precache-manifest.2599750d4180043f643a38ea5fece5e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4cf8d40c07c40adf0fc2",
+    "url": "/static/js/main.06cec21f.chunk.js"
+  },
+  {
+    "revision": "c61f13c766b27736bbcc",
+    "url": "/static/js/2.6489e074.chunk.js"
+  },
+  {
+    "revision": "4cf8d40c07c40adf0fc2",
+    "url": "/static/css/main.66c4dead.chunk.css"
+  },
+  {
+    "revision": "6666764fea6cf9ea2dcee2b95b70004f",
+    "url": "/index.html"
+  }
+];
