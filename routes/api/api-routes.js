9f80067c75a314c0a6bb9055f4f353bd3c2698fb
HaifaSeyam <h.abuamro@hotmail.com>
2019-04-30 07:54:56 -0500

    new files

--- routes/api/api-routes.js
+++ routes/api/api-routes.js
@@ -66,5 +66,5 @@ module.exports = function(app) {
 
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../client/build/index.html"));
-    });
+        });
 }
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-
-const path = require('path');
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,12 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static("client/build"));
 }
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
-
 
 // Start the API server
 app.listen(PORT, function() {
