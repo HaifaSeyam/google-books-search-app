384edecebc90ffec73964a3ff0969687870665e8
HaifaSeyam <h.abuamro@hotmail.com>
2019-04-30 08:31:09 -0500

    add app

--- routes/api/api-routes.js
+++ routes/api/api-routes.js
@@ -64,7 +64,13 @@ module.exports = function(app) {
             
     });
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../client/build/index.html"));
-        });
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    //     });
+
+    app.get('*', function (req, res) {
+        const index = path.join(__dirname, 'build', 'index.html');
+        res.sendFile(index);
+      });
+      
 }
