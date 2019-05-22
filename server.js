7e8e2243424283c711a059627d288359f37a689e
HaifaSeyam <h.abuamro@hotmail.com>
2019-05-22 05:10:56 -0500

    minor change to server.js

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-require("./routes/api/api-routes")(app);
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build/index.html")));
+  app.use(express.static("client/build"));
 }
 
+// Add routes, both API and view
+require("./routes/api/api-routes")(app);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
